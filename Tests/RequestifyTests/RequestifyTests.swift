f000c0b9ce3990a54aa115f7e4ce1f0bdbbd6b13
insub <vb15@kakao.com>
2024-09-22 18:01:37 +0900

    Update RequestifyTests.swift

--- Tests/RequestifyTests/RequestifyTests.swift
+++ Tests/RequestifyTests/RequestifyTests.swift
@@ -1,5 +1,8 @@
 import XCTest
 import Alamofire
+#if canImport(UIKit)
+import UIKit
+#endif
 @testable import Requestify
 
 final class RequestifyTests: XCTestCase {
@@ -22,5 +25,45 @@ final class RequestifyTests: XCTestCase {
         let builder = Requestify().setParameters(parameters)
         
         XCTAssertEqual(builder.parameters?["key"] as? String, "value", "Parameters should be set correctly")
+    }
+    
+    
+    func testSetHeaders() {
+        let headers: HTTPHeaders = ["Authorization": "Bearer token123"]
+        let builder = Requestify().setHeaders(headers)
+        
+        XCTAssertEqual(builder.headers?["Authorization"], "Bearer token123", "Headers should be set correctly")
+    }
+    
+    func testSetPrintLog() {
+        let builder = Requestify().setPrintLog(false)
+        
+        XCTAssertFalse(builder.printLog, "Print log should be set to false")
+    }
+    
+    func testSetPrintResponse() {
+        let builder = Requestify().setPrintResponse(false)
+        
+        XCTAssertFalse(builder.printResponse, "Print response should be set to false")
+    }
+    
+    #if canImport(UIKit)
+    func testAddImages() {
+        let image = UIImage(systemName: "star") // 사용 가능한 더미 이미지
+        let builder = Requestify().addImages([image], withName: "image")
+        
+        XCTAssertFalse(builder.formDataBuilders.isEmpty, "Form data builders should not be empty when images are added")
+    }
+    #endif
+    
+    func testAddObject() {
+        struct DummyObject: Encodable {
+            let name: String
+            let age: Int
+        }
+        let dummyObject = DummyObject(name: "John", age: 25)
+        let builder = Requestify().addObject(dummyObject, withName: "user")
+        
+        XCTAssertNotNil(builder.object, "Encodable object should be added correctly")
     }
 }
